5d0882f972ca0bef848688c65f2398e3be01f991
ramarora00 <ramarora0075@gmail.com>
2026-07-05 15:44:23 +0530

    chore: remove unused file and associated references as of typescript files

--- client/src/shared/utils/classNames.ts
+++
@@ -1,6 +0,0 @@
-// client/src/shared/utils/classNames.ts
-/**
- * TypeScript wrapper for the cn utility.
- * Re‑exports the JavaScript implementation so TypeScript imports resolve.
- */
-export { cn } from './classNames';
